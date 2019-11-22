86284140e639eab53ef8ebf7f4663b8755f0c373
Darius Mix <d.mix@dvz-mv.de>
2019-11-22 11:44:33 +0100

    Admincontroller eingefügt

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using it_shop_app.Models;
+
+namespace it_shop_app.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Nutzer()
+        {
+            return View();
+        }
+
+        public IActionResult Artikel()
+        {
+            return View();
+        }
+    }
+}
