8a9e1c5bed08cc0602618e30260c510311c3cee6
Darius Mix <d.mix@dvz-mv.de>
2019-11-25 09:22:56 +0100

    Dokumente bearbeitet und fehlende Importe ergänzt

--- Models/Bestellung.cs
+++ Models/Bestellung.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace it_shop_app.Models
 {
