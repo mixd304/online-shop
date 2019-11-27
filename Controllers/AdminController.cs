07c4a8f9c095647a5a25425e0717f0970ede8730
Tim Vinzing <t.vinzing@dvz-mv.de>
2019-11-27 10:42:24 +0100

    Merkmale können jetzt hinzugefügt

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -34,6 +34,58 @@ namespace it_shop_app.Controllers {
             return View("Views/Admin/Artikel/Edit.cshtml", await _context.Artikel.FirstOrDefaultAsync(m => m.ID == id));
         }
 
+        public async Task<IActionResult> Merkmal_Create(int id) {
+
+            await _context.Merkmale.ToListAsync();
+
+            var artikel = await _context.Artikel
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (artikel == null)
+            {
+                return NotFound();
+            }
+
+            var model = new CreateArtikelViewModel();
+            model.artikel = artikel;
+
+            return View("Views/Admin/Artikel/CreateMerkmal.cshtml", model);
+        } 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Merkmal_Create(int id, CreateArtikelViewModel model) {
+
+            await _context.Merkmale.ToListAsync();
+
+            var artikel = await _context.Artikel
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (artikel == null)
+            {
+                return NotFound();
+            }
+
+            model.artikel = artikel;
+
+            Console.WriteLine("=====================TEST=======================");
+            Console.WriteLine("Artikel Bezeichnung:    " + model.artikel.Bezeichnung);
+            Console.WriteLine("Artikel Beschreibung:   " + model.artikel.Beschreibung);
+            Console.WriteLine("Merkmal Bezeichnung:    " + model.merkmal.Bezeichnung);
+            Console.WriteLine("Merkmal Wert:           " + model.merkmal.Wert);            
+
+            model.merkmal.Artikel_ID = model.artikel.ID;
+            Console.WriteLine();
+
+            Console.WriteLine("Artikel PK:             " + model.artikel.ID);
+            Console.WriteLine("Merkmal Fremdschlüssel: " + model.merkmal.Artikel_ID);
+
+            _context.Add(model.merkmal);
+            await _context.SaveChangesAsync();
+
+            return View("Views/Admin/Artikel/CreateMerkmal.cshtml", model);
+        }
+
         public IActionResult Artikel_Create()
         {        
             return View("Views/Admin/Artikel/Create.cshtml");
