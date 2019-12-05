12569af2b574df2bc0359a4ea744bb714ae6bab7
Darius Mix <d.mix@dvz-mv.de>
2019-12-05 13:49:48 +0100

    Noch mehr Bugfixes

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -84,6 +84,7 @@ namespace it_shop_app.Controllers {
          */
         public async Task<IActionResult> Merkmal_Create(int id) {
 
+            await _context.MerkmalBezeichnungen.ToListAsync();
             await _context.Merkmale.ToListAsync();
 
             var artikel = await _context.Artikel
@@ -117,8 +118,8 @@ namespace it_shop_app.Controllers {
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Merkmal_Create(int id, CreateArtikelViewModel model) {
 
+            await _context.MerkmalBezeichnungen.ToListAsync();
             await _context.Merkmale.ToListAsync();
-
             var artikel = await _context.Artikel
                 .FirstOrDefaultAsync(m => m.ID == id);
 
@@ -127,8 +128,20 @@ namespace it_shop_app.Controllers {
                 return NotFound();
             }
 
-            model.artikel = artikel;           
+            model.artikel = artikel;
 
+            MerkmalBezeichnung merkmalBezeichnung = await _context.MerkmalBezeichnungen.FirstOrDefaultAsync(mb => mb.Bezeichnung == model.merkmalBezeichnung.Bezeichnung);
+            if (merkmalBezeichnung == null)
+            {
+                _context.Add(model.merkmalBezeichnung);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                model.merkmalBezeichnung = merkmalBezeichnung;
+            }
+
+            model.merkmal.Bezeichnung_ID = model.merkmalBezeichnung.ID;
             model.merkmal.Artikel_ID = model.artikel.ID;
 
             _context.Add(model.merkmal);
@@ -167,16 +180,32 @@ namespace it_shop_app.Controllers {
         public async Task<IActionResult> Create(CreateArtikelViewModel model)
         {
             if (ModelState.IsValid)
-            {           
-                _context.Add(model.kategorie);
-                await _context.SaveChangesAsync();
-
-                _context.Add(model.merkmalBezeichnung);
-                await _context.SaveChangesAsync();
+            {
+                Kategorie kategorie = await _context.Kategorien.FirstOrDefaultAsync(k => k.Bezeichnung == model.kategorie.Bezeichnung);
+                if (kategorie == null)
+                { 
+                    _context.Add(model.kategorie);
+                    await _context.SaveChangesAsync();
+                } 
+                else
+                {
+                    model.kategorie = kategorie;
+                }
 
                 model.artikel.Kategorie_ID = model.kategorie.ID;
                 _context.Add(model.artikel);
                 await _context.SaveChangesAsync();
+
+                MerkmalBezeichnung merkmalBezeichnung = await _context.MerkmalBezeichnungen.FirstOrDefaultAsync(mb => mb.Bezeichnung == model.merkmalBezeichnung.Bezeichnung);
+                if (merkmalBezeichnung == null)
+                {
+                    _context.Add(model.merkmalBezeichnung);
+                    await _context.SaveChangesAsync();
+                } 
+                else
+                {
+                    model.merkmalBezeichnung = merkmalBezeichnung;
+                }
 
                 model.merkmal.Bezeichnung_ID = model.merkmalBezeichnung.ID;
                 model.merkmal.Artikel_ID = model.artikel.ID;
